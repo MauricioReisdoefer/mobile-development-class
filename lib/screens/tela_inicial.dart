f78c38674edc701d899f1c106075eb5bbbfbb418
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-09 14:49:04 -0300

    FEAT: Tela inicial e List View com lista de personagens

---
+++ lib/screens/tela_inicial.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+List<String> images = ["locations.png", "episodes.png", "characters.png"];
+List<String> titles = ["Locations", "Episodes", "Characters"];
+
+class TelaInicial extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child:Text("Rick and Morty API Consumer",
+          style: TextStyle(color: Colors.white),
+          )),
+        leading: Icon(Icons.rocket, color: Colors.white), 
+        backgroundColor: const Color.fromARGB(255, 36, 106, 163),
+        ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: 3,
+          itemBuilder:(context, index) {
+            return Center(
+              child: Padding(
+                padding: EdgeInsetsGeometry.directional(bottom: 35.00),
+                child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                    child: Image.asset("assets/${images[index]}", height: 150, width: 150),
+                    onTap: () => {
+                      debugPrint("CLICOU EM ${titles[index]}")
+                    }),
+                    SizedBox(height: 10),
+                    Text(
+                      titles[index],
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),),
+            );
+          },
+        )
+      ),
+    );
+  }
+}
---
+++ lib/widgets/characters.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_dev_class/core/models/character_model.dart';
+
+class Characters extends StatelessWidget {
+  final List<Character> charList;
+
+  Characters({required this.charList});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: charList.length,
+      itemBuilder: (context, index) {
+        final character = charList[index];
+        return ListTile(
+          title: Text(character.name),
+          subtitle: Text(character.gender),
+        );
+      },
+    );
+  }
+}
