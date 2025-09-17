b25fac60aad0e563fcdd92298d72fb79f576aa5b
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-17 14:34:56 -0300

    FEAT: Adicionando requisicao para tela personagens

--- lib/screens/tela_personagens.dart
+++ lib/screens/tela_personagens.dart
@@ -1,17 +1,57 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_dev_class/core/models/character_model.dart';
 import 'package:mobile_dev_class/widgets/characters.dart';
 import 'package:mobile_dev_class/widgets/appbar.dart';
+import 'package:http/http.dart' as http;
 
-class TelaPersonagens extends StatelessWidget {
+// https://rickandmortyapi.com/api/character
+
+class TelaPersonagens extends StatefulWidget {
+  @override
+  State<TelaPersonagens> createState() => _TelaPersonagensState();
+}
+
+class _TelaPersonagensState extends State<TelaPersonagens> {
+  List<Character> characters = [];
+
+  Future<void> getPageData() async {
+    final response = await http.get(Uri.parse("https://rickandmortyapi.com/api/character"));
+
+    if(response.statusCode != 200)
+    {
+      throw new Exception("Falha ao carregar os dados");
+    }
+
+    final body_decoded = jsonDecode(response.body);
+    final results = body_decoded["results"];
+    if(results != null)
+    {
+      for (final result in results)
+      {
+        characters.add(new Character.fromJson(result));
+      };
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: RickAppBar(),
-        body: CharactersList(
-          charList: [Character(id: 0, name: "Rick Sanchez C-137", status: "Alive", species: "Human", type: "", gender: "Male")],
-        ),
+        body: FutureBuilder(
+          future: getPageData(), 
+          builder:(context, snapshot) {
+            if(1 == 1){
+              return 
+              CharactersList(
+                charList: characters
+              );
+          } else {
+            return Text("Vazio");
+          }
+          },
+        )
     );
   }
-  
 }
