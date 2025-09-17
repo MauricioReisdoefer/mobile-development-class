7a2dcb7c06f3e261142a852a18e8ec107a6be154
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-17 14:41:54 -0300

    FEAT: Criando info model

---
+++ lib/core/models/info_model.dart
@@ -0,0 +1,22 @@
+class InfoModel {
+  int count;
+  int pages;
+  String? next;
+  String? prev;
+
+  InfoModel({required this.count, required this.pages, this.next, this.prev});
+
+  factory InfoModel.fromJson(Map<String, dynamic> json) => InfoModel(
+    count: json["count"], 
+    pages: json["pages"],
+    next: json["next"],
+    prev: json["prev"]
+  );
+
+  Map<String, dynamic> toJson() => {
+      "count" : count,
+      "pages": pages,
+      "next": next,
+      "prev": prev
+  };
+}
